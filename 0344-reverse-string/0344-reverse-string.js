073b139dfa287a34d196b473c89a0366c8f70914
fidhafathima07 <nfidhafathima03@gmail.com>
2026-09-04 10:33:18 +0530

    Time: 0 ms (100%), Space: 61 MB (65.6%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s.reverse()
+};
+    let arr=["h","e","l","l","o"]
+    reverseString(arr)
+    console.log(arr)
+    
