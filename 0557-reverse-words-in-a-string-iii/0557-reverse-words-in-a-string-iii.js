deb52c662a9431e7a91c834525797bbbc64b59e4
fidhafathima07 <nfidhafathima03@gmail.com>
2026-09-04 10:57:55 +0530

    Time: 11 ms (58.36%), Space: 60.8 MB (53.66%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function reverseWords(s) {
+  return s
+    .split(" ")
+    .map(word => word.split("").reverse().join(""))
+    .join(" ");
+}
+console.log(reverseWords("Mr Ding"))
